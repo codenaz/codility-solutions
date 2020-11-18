f0e500417cf85260cdeef2fbc3a2f509b7d285e3
Nwakwoke Patrick Nnaemeka <princenazio2@gmail.com>
2020-11-18 11:43:12 +0100

    Create count-factors.js

---
+++ count-factors.js
@@ -0,0 +1,17 @@
+function solution(N) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    let factors = 0;
+    let i = 1;
+    while((i * i) < N) {
+        if (N % i === 0) {
+            factors += 2;
+        }
+        i++;
+    }
+    
+    if (i * i === N) {
+        factors++;
+    }
+    
+    return factors;
+}
